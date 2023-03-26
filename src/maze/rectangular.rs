f2782b431dc49519a379ba708f8e1b7452331fda
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-03-25 20:18:48 -0400

    Add gen_grid
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- src/maze/rectangular.rs
+++ src/maze/rectangular.rs
@@ -1,24 +1,49 @@
-use rand::{Rng, SeedableRng};
+use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
-use std::fmt;
+use rusttype::Point;
+use std::{collections::HashMap, fmt};
 
-#[derive(Debug)]
+struct GridCell {
+    links: HashMap<Point<i32>, GridCell>,
+}
+
 pub struct Rectangular {
     width: i32,
     height: i32,
     rng: ChaCha8Rng,
+    map: HashMap<Point<i32>, GridCell>,
 }
 
 impl Rectangular {
     pub fn new(width: i32, height: i32, seed: u64) -> Rectangular {
         let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(seed);
-        println!("Random f32: {}", rng.gen::<f32>());
-        Rectangular { width, height, rng }
+        let mut map = gen_grid(width, height);
+        Rectangular {
+            width,
+            height,
+            rng,
+            map,
+        }
     }
+}
+
+fn gen_grid(width: i32, height: i32) -> HashMap<Point<i32>, GridCell> {
+    let mut map = HashMap::new();
+    for y in 0..height {
+        for x in 0..width {
+            map.insert(
+                Point { x, y },
+                GridCell {
+                    links: HashMap::new(),
+                },
+            );
+        }
+    }
+    map
 }
 
 impl fmt::Display for Rectangular {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Rectangular({} x {})", self.width, self.height)
+        writeln!(f, "Rectangular({} x {})", self.width, self.height)
     }
 }
