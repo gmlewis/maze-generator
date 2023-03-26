1d84a1f5e6927d13a647491d57c629c82ec707c0
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2023-03-25 20:43:23 -0400

    Add GridCell methods
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- src/maze/cell.rs
+++ src/maze/cell.rs
@@ -1,14 +1,51 @@
 use rusttype::Point;
 use std::collections::HashMap;
 
-pub struct Cell {
-    links: HashMap<Point<i32>, Cell>,
+pub struct GridCell {
+    key: Point<i32>,
+    links: HashMap<Point<i32>, GridCell>,
 }
 
-impl Cell {
-    pub fn new() -> Cell {
-        Cell {
-            links: HashMap::new(),
-        }
+impl GridCell {
+    pub fn new(key: Point<i32>) -> GridCell {
+        let links = HashMap::new();
+        GridCell { key, links }
+    }
+
+    pub fn north(&self) -> Option<&GridCell> {
+        let k = Point {
+            x: self.key.x,
+            y: self.key.y - 1,
+        };
+        self.links.get(&k)
+    }
+
+    pub fn south(&self) -> Option<&GridCell> {
+        let k = Point {
+            x: self.key.x,
+            y: self.key.y + 1,
+        };
+        self.links.get(&k)
+    }
+
+    pub fn east(&self) -> Option<&GridCell> {
+        let k = Point {
+            x: self.key.x - 1,
+            y: self.key.y,
+        };
+        self.links.get(&k)
+    }
+
+    pub fn west(&self) -> Option<&GridCell> {
+        let k = Point {
+            x: self.key.x + 1,
+            y: self.key.y,
+        };
+        self.links.get(&k)
+    }
+
+    pub fn neighbors(&self) -> Vec<&GridCell> {
+        let dirs = vec![self.north(), self.south(), self.east(), self.west()];
+        dirs.into_iter().flatten().collect()
     }
 }
--- src/maze/rectangular.rs
+++ src/maze/rectangular.rs
@@ -1,4 +1,4 @@
-use crate::maze::cell::Cell;
+use crate::maze::cell::GridCell;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 use rusttype::Point;
@@ -8,13 +8,13 @@ pub struct Rectangular {
     width: i32,
     height: i32,
     rng: ChaCha8Rng,
-    map: HashMap<Point<i32>, Cell>,
+    map: HashMap<Point<i32>, GridCell>,
 }
 
 impl Rectangular {
     pub fn new(width: i32, height: i32, seed: u64) -> Rectangular {
-        let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(seed);
-        let mut map = gen_grid(width, height);
+        let rng = rand_chacha::ChaCha8Rng::seed_from_u64(seed);
+        let map = gen_grid(width, height);
         Rectangular {
             width,
             height,
@@ -24,11 +24,12 @@ impl Rectangular {
     }
 }
 
-fn gen_grid(width: i32, height: i32) -> HashMap<Point<i32>, Cell> {
+fn gen_grid(width: i32, height: i32) -> HashMap<Point<i32>, GridCell> {
     let mut map = HashMap::new();
     for y in 0..height {
         for x in 0..width {
-            map.insert(Point { x, y }, Cell::new());
+            let k = Point { x, y };
+            map.insert(k, GridCell::new(k));
         }
     }
     map
